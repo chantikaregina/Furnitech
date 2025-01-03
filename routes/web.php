7a82b0c25b539bae05bbd6f4cfa87c27e8989b0d
olivaazarroh <azarroholiva@gmail.com>
2025-01-03 10:39:02 +0700

    hd

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,16 @@
 <?php
 
 use App\Http\Controllers\Auth\UserLoginController;
+use App\Http\Controllers\Frontend\HomeController;
 use Illuminate\Support\Facades\Route;
+
 
 Route::get('/', function () {
     return view('welcome');
 });
 
+
 Route::get('/login', [UserLoginController::class, 'login'])->name('login');
 
+
+Route::get('/', [HomeController::class, 'home'])->name('home');
