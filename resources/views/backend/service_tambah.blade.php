4670417b468b8a80d2a15c4ee44d8c9183663d3d
chantikaregina <reginachantika50@gmail.com>
2025-01-08 13:58:56 +0700

    ft

--- resources/views/backend/service_tambah.blade.php
+++ resources/views/backend/service_tambah.blade.php
@@ -39,6 +39,15 @@
                                     @enderror
                                 </div>
                             </div>
+                            <div class="mb-3">
+                                <label for="foto" class="form-label">Foto</label>
+                                <input type="file" class="form-control" id="foto" name="foto">
+                                <div class="text-danger">
+                                    @error('foto')
+                                        {{ $message }}
+                                    @enderror
+                                </div>
+                            </div>
                             <button type="submit" class="btn btn-primary">Simpan</button>
                         </form>
                     </div>
