769a1d3701dea69a2bc6fbf56581af166ac9c8b0
olivaazarroh <azarroholiva@gmail.com>
2025-01-14 11:35:40 +0700

    fr

--- database/migrations/2025_01_10_082140_create_abouts_table.php
+++ database/migrations/2025_01_10_082140_create_abouts_table.php
@@ -14,8 +14,8 @@ return new class extends Migration
         Schema::create('abouts', function (Blueprint $table) {
             $table->integer('id_about')->primary()->autoIncrement();
             $table->text('background');
-            $table->text('visi', 255);
-            $table->text('misi', 255);
+            $table->text('visi');
+            $table->text('misi');
             $table->string('foto1')->nullable();
             $table->string('foto2')->nullable();
             $table->timestamps();
